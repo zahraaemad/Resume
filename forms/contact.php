4135480fe9178df669793e7044269fa5c6644e7e
Zahraa <42125987+zahraaemad@users.noreply.github.com>
2021-08-30 14:23:51 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,13 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'zahraa95emad@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+//     die( 'Unable to load the "PHP Email Form" Library!');
+        include( $php_email_form );
   }
 
   $contact = new PHP_Email_Form;
